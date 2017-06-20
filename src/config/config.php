ef2cb008f47c0c2656957aa925eb7e92cb705f92
hyperbolaa <yuchong321@126.com>
2017-06-20 17:34:00 +0800

    添加日志

--- src/config/config.php
+++ src/config/config.php
@@ -1,6 +1,18 @@
 <?php
 return [
+	/*
+	 * Debug 模式，bool 值：true/false
+	 * 当值为 false 时，所有的日志都不会记录
+	 */
 	'debug' =>  true,
+	/*
+	 * level: 日志级别，可选为：debug/info/notice/warning/error/critical/alert/emergency
+	 * file：日志文件位置(绝对路径!!!)，要求可写权限
+	 */
+	'log'=>[
+		'level' => 'debug',
+		'file'  => storage_path('logs/wechatpay.log'),
+	],
 	'app'=>[
 		'app_id'        => '',//微信支付移动APPID
 		'merchant_id'   => '',//微信支付分配的商户号
@@ -21,16 +33,5 @@ return [
 		'key'           => '',//支付签名使用
 		'notify_url'    => '',//异步接收微信支付结果通知的回调地址
 		'device_info'   => 'WEB',//设备号
-	],
-	/*
-	 * 日志配置
-	 *
-	 * level: 日志级别，可选为：
-	 *                 debug/info/notice/warning/error/critical/alert/emergency
-	 * file：日志文件位置(绝对路径!!!)，要求可写权限
-	 */
-	'log'=>[
-		'level' => 'debug',
-		'file'  => storage_path('logs/wechatpay.log'),
 	]
 ];
