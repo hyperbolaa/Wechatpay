5062588d9a15d9b73154295b828290ac2b974f8b
hyperbolaa <yuchong321@126.com>
2018-07-05 10:23:15 +0800

    sage

--- src/Hyperbolaa/Wechatpay/Sdk/helpers.php
+++ src/Hyperbolaa/Wechatpay/Sdk/helpers.php
@@ -30,6 +30,7 @@ function array2xml($arr, $root = 'xml')
  */
 function xml2array($xml)
 {
+	libxml_disable_entity_loader(true);
 	return json_decode(json_encode(simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA)), true);
 }
 
